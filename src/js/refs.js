aeb4902da3e3cc47aaf3aa888f9f28a0c1580b20
VitaPerevoznikova <frayvita@gmail.com>
2023-09-19 11:52:34 +0300

    update PJ

--- src/js/refs.js
+++
@@ -1,5 +0,0 @@
-export const refs = {
-    searchForm: document.querySelector('.search-form'),
-    galleryContainer: document.querySelector('.gallery'),
-    btnLoadMore: document.querySelector('.load-more'),
-  };
